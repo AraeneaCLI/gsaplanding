88cb579e15d3bb21b5e223bf9e89b35a25b869fa
Yash Mankar <yash.mankar10122003@gmail.com>
2024-06-13 01:12:54 +0530

    Image Updates

--- app/gsap/fliptext.js
+++ app/gsap/fliptext.js
@@ -5,7 +5,8 @@ import Image from 'next/image';
 import { gsap } from "gsap";
 import styles from '@/app/styles/home.module.scss';
 
-import {Loading} from '@/app/img/loading.svg';
+import {fan} from '@/app/img/fan.png';
+import {osm} from '@/app/img/osmanthus.png';
 
 const FlipText = () => {
     const aRef = useRef();
@@ -15,18 +16,21 @@ const FlipText = () => {
     const hzRef = useRef();
     const hzmovRef = useRef();
     const nmorf = useRef();
+    const uptodwn = useRef();
 
     useEffect(() => {
-        gsap.to(aRef.current, { scaleY: 1, duration: 1 , repeat: 1, yoyo: true, ease: 'elastic'});
-        gsap.to(iRef.current, { y: -1, duration: 1 , repeat: -1, yoyo: true, ease: 'power1.inOut'});
-        gsap.to(mRef.current, { x: 15, duration: 1 , repeat: -1, yoyo: true, repeatDelay: 2});
-        gsap.to(hzRef.current, { x: 15, duration: 1 , repeat: -1, yoyo: true});
-        gsap.fromTo( hzmovRef,{ x: -40}, { x: 40});
+        gsap.to(aRef.current, { y: -45, duration: 2 , repeat: 1, yoyo: true, ease: 'elastic'});
+        gsap.to(iRef.current, { scaleY: -1, duration: 2, repeat: -1, repeatDelay: 3, yoyo: true, ease: 'power1.inOut'});
+        gsap.fromTo( mRef.current,{ x: 0}, { x: 100, duration: 1 , repeat: 1, yoyo: true});
+        gsap.to(hzRef.current, { x: -45, duration: 0.5 , repeat: 1, yoyo: true});
+        gsap.fromTo( hzmovRef.current,{ x: -10}, { x: 10, duration: 1 , repeat: 1, yoyo: true});
         gsap.to(nmorf, {
           duration: 2,
           text: "N",
           ease: "power1.inOut",
         });
+        gsap.fromTo(uptodwn.current, { y: -100 }, {y: 0, duration: 0.5});
+        gsap.to(uptodwn.current, {}); 
         }, []);
 
     return(
@@ -35,12 +39,15 @@ const FlipText = () => {
         <div className={styles.lftcontainer}>
           <div className={styles.fontele} ref={aRef}>A</div>
           <div>
+            <div>
+              <Image src={fan} alt="" width={50} height={50} />
+            </div>
             <div className={styles.fontele} ref={nmorf}>n</div>
           </div>
           <div className={styles.fontele} ref={iRef}>i</div>
           <div className={styles.fontele} ref={mRef}>m</div>
           <div className={styles.fontele} ref={hzmovRef}>a</div>
-          <div className={styles.fontele} ref={hzmovRef}>t</div>
+          <div className={styles.fontele} ref={uptodwn}>t</div>
           <div className={styles.fontele}>e</div>
         </div>
 
@@ -54,6 +61,7 @@ const FlipText = () => {
           <div className={styles.fontele} ref={iRef}>i</div>
           <div className={styles.fontele}>n</div>
           <div className={styles.fontele}>g</div>
+          <Image src={osm} alt='' width={30} height={30}/>
         </div>
         </div>
 
